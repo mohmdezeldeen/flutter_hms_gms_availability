6db8ec00b539629d794d8f20a23a4384162fc6c4
Ivan Kurkin <ikurkin@tennisi.it>
2025-03-18 15:40:53 +0500

    remove deprecated android plugin "Registrar"

--- android/src/main/kotlin/com/chimou/flutter_hms_gms_availability/FlutterHmsGmsAvailabilityPlugin.kt
+++ android/src/main/kotlin/com/chimou/flutter_hms_gms_availability/FlutterHmsGmsAvailabilityPlugin.kt
@@ -6,7 +6,6 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 import android.content.Context
 import com.huawei.hms.api.ConnectionResult
 import com.huawei.hms.api.HuaweiApiAvailability
